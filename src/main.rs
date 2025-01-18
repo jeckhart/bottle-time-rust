42cc750a8f18be17526851bd5c5857ebbe743d41
John Eckhart <jeckhart@hey.com>
2025-01-18 16:30:47 -0500

    First example program that actually works

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+use embassy_time::{Duration, Timer};
+use esp_idf_svc::log::EspLogger;
+use esp_idf_svc::sys::link_patches;
+use esp_idf_svc::hal::task::block_on;
+
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+    // Necessary for linking patches to the runtime
+    link_patches();
 
-    // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    // Initialize the logger
+    EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    // Run the asynchronous main function
+    block_on(async_main());
+}
+
+async fn async_main() {
+    loop {
+        log::info!("Asynchronous task running...");
+        Timer::after(Duration::from_secs(1)).await;
+    }
 }
