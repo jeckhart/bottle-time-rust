94fd63381901641f4d508b92a75fb24888dacf6f
John Eckhart <jeckhart@hey.com>
2025-01-19 17:04:35 -0500

    Use embassy, OnceLock, RefCel, etc to enable us to asyncronously control an LED turning on and off

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,67 @@
+use std::cell::RefCell;
+use embassy_executor::Spawner;
+use embassy_sync::once_lock::OnceLock;
 use embassy_time::{Duration, Timer};
 use esp_idf_svc::log::EspLogger;
 use esp_idf_svc::sys::link_patches;
-use esp_idf_svc::hal::task::block_on;
+
+use esp_idf_hal::prelude::*;
 
 use esp_backtrace as _;
+use esp_idf_hal::gpio::{AnyIOPin, IOPin, Input, Output, PinDriver};
 use esp_println as _;
 
-fn main() {
+static BUTTON_PIN: OnceLock<&mut RefCell<PinDriver<AnyIOPin, Input>>> = OnceLock::new();
+static LED_PIN: OnceLock<&mut RefCell<PinDriver<AnyIOPin, Output>>> = OnceLock::new();
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
     // Necessary for linking patches to the runtime
     link_patches();
 
     // Initialize the logger
     EspLogger::initialize_default();
+    defmt::println!("Starting async main");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    // Initialize the shared LED pin
+    defmt::println!("Setting up LED Pin");
+    let led_pin = peripherals.pins.gpio2;
+    let led_pin_driver = Box::new(RefCell::new(PinDriver::output(led_pin.downgrade()).unwrap()));
+    let _res = LED_PIN.init(Box::leak(led_pin_driver));
+    defmt::println!("Done setting up LED Pin");
+
+    // Initialize the shared LED pin
+    defmt::println!("Setting up BUTTON Pin");
+    let button_pin = peripherals.pins.gpio4;
+    let button_pin_driver = Box::new(RefCell::new(PinDriver::input(button_pin.downgrade()).unwrap()));
+    let _res = BUTTON_PIN.init(Box::leak(button_pin_driver));
+    defmt::println!("Done setting up BUTTON Pin");
 
     // Run the asynchronous main function
-    block_on(async_main());
+    spawner.spawn(blinky()).unwrap();
+    spawner.spawn(async_main()).unwrap();
 }
 
+#[embassy_executor::task]
+async fn blinky() {
+    {
+        let led_pin = LED_PIN.get().await;
+        defmt::println!("Setting up LED blinky task on pin {:?}", led_pin.borrow().pin());
+    }
+    loop {
+        defmt::println!("Looping on LED blinky task");
+        let led_pin = LED_PIN.get().await;
+        led_pin.borrow_mut().set_high().unwrap();
+        Timer::after(Duration::from_millis(1000)).await;
+        let led_pin = LED_PIN.get().await;
+        led_pin.borrow_mut().set_low().unwrap();
+        Timer::after(Duration::from_millis(1000)).await;
+    }
+}
+
+#[embassy_executor::task]
 async fn async_main() {
     defmt::println!("Starting async loop");
     defmt::info!("Waiting for EspLog to be ready");
